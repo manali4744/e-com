745522fb0172ad9e82a99cef4156c604b2acbe7b
manali <manali.empiric98@gmail.com>
2023-04-05 17:44:54 +0530

    add space

--- new_login/new_login/product/forms.py
+++ new_login/new_login/product/forms.py
@@ -3,6 +3,7 @@ from .models import Order
 from django import forms
 import re
 from django.core.exceptions import ValidationError
+
 class OrderForm(ModelForm):
     customer = forms.CharField(max_length= 255)
     product = forms.CharField(max_length= 255)
